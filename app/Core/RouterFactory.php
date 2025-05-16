338a7a4e107297e5e6a024946faf94b90dad9502
AlesGit85 <a.zita@post.cz>
2025-05-16 16:18:09 +0200

    zmena routefactory a dalsi

--- app/Core/RouterFactory.php
+++ app/Core/RouterFactory.php
@@ -1,15 +1,27 @@
 <?php
 declare(strict_types=1);
 namespace App\Core;
+
 use Nette;
 use Nette\Application\Routers\RouteList;
+
 final class RouterFactory
 {
-        use Nette\StaticClass;
-        public static function createRouter(): RouteList
-        {
-                $router = new RouteList;
-                $router->addRoute('<presenter>/<action>[/<id>]', 'Faktura:default');
-                return $router;
-        }
+    use Nette\StaticClass;
+
+    public static function createRouter(): RouteList
+    {
+        $router = new RouteList;
+        
+        // Definování základních routovacích pravidel
+        $router->addRoute('dodavatel', 'Dodavatel:default');
+        $router->addRoute('odberatel', 'Odberatel:default');
+        $router->addRoute('faktura', 'Faktura:default');
+        $router->addRoute('', 'Faktura:default'); // výchozí route
+        
+        // Fallback route pro další presenter a akce
+        $router->addRoute('<presenter>/<action>[/<id>]', 'Faktura:default');
+        
+        return $router;
+    }
 }
